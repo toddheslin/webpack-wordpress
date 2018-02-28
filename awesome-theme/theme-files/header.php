79b0370667920fb2d1c7c761aadb5feffaa36040
Todd Heslin <todd@beingremarkable.me>
2018-02-28 16:53:09 +0800

    Fixed header bundle reference

--- awesome-theme/theme-files/header.php
+++ awesome-theme/theme-files/header.php
@@ -17,8 +17,8 @@
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-	<script type="text/javascript" src="<?php get_template_directory_uri() ?>/bundle.js"></script>
 	<?php endif; ?>
+	<script type="text/javascript" src="<?php echo get_template_directory_uri() ?>/bundle.js"></script>
 	<?php wp_head(); ?>
 </head>
 
